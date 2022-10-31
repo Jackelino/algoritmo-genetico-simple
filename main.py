ce759b5f0604df26a203248116df1725667a3da9
Jackelino <codigo.gratuito2019@gmail.com>
2022-10-31 14:46:23 -0600

    definicion de parametros como constantes

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ TYPE_MUTATIONS = {
     "BIT_MENOS_SIGNIFICATIVO": 1
 }
 
+TYPE_OPTIMIZATION = {
+    "MAX": "MAX",
+    "MIN": "MIN"
+}
+
+POPULATION = 10
+CROSSOVER_RATE = 0.5
+MUTATION_RATE = 0.1
+GENERATION_NUMBER = 10
+
 
 def main():
     listChromosome = []
@@ -22,15 +32,8 @@ def main():
     c2 = Chromosome(-5.11, 5.12, TYPE_VARIABLE.get("FLOTANTE"), 2)
     listChromosome.append(c1)
     print("numero de bits a utilizar:", c2.numberBits)
-
-    """indi = Individual(listChromosome)
-    indi2 = Individual(listChromosome, [0, 0, 0])
-    print(indi2.values)
-    print(indi2.maps)
-    print(indi2.adn)
-    print(indi2.fitness)"""
     # se crea la primera generacion
-    generation = Generation(6, "MAX", 0.5, 0.1, 4, listChromosome, 1)
+    generation = Generation(POPULATION, TYPE_OPTIMIZATION.get("MAX"), CROSSOVER_RATE, MUTATION_RATE, 4, listChromosome, 1)
     listGeneration.append(generation)
 
     print("Numero de generacion: ", generation.nG)
@@ -42,11 +45,11 @@ def main():
     print(generation.eliteIndividual.values, "\t", generation.eliteIndividual.maps, "\t",
           generation.eliteIndividual.adn, "\t", generation.eliteIndividual.fitness)
 
-    paro = 10
     i = 0
     bestGeneration = 1
-    while i < paro - 1:
-        generqacionNew = Generation(6, "MAX", 0.5, 0.1, 4, listChromosome, (i + 2), listGeneration[i],
+    while i < GENERATION_NUMBER - 1:
+        generqacionNew = Generation(POPULATION, TYPE_OPTIMIZATION.get("MAX"), CROSSOVER_RATE, MUTATION_RATE, 4, listChromosome, (i + 2),
+                                    listGeneration[i],
                                     TYPE_MUTATIONS.get("ALEATORIA"))
         listGeneration.append(generqacionNew)
         print("Numero de generacion: ", generqacionNew.nG)
